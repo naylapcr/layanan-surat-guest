5fc4a49c38bc0f0cf1301d493e5d947799f4ea7e
naylapcr <nayla24si@gmail.com>
2025-11-04 08:43:18 +0700

    tambah floating wa

--- resources/views/layouts/guest/app.blade.php
+++ resources/views/layouts/guest/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Warga - Layanan Desa</title>
+    <title>Layanan Desa</title>
     @include('layouts.guest.css')
 
 </head>
